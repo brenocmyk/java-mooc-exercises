1dc74b5fed2ca3f99695537889cc05cf4f2d7556
breno <breno.aguiarba06@gmail.com>
2026-03-05 21:27:58 -0300

    update nos exercicios

--- part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -5,6 +5,16 @@ public class ComparingNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Enter the first number: ");
+        int num1 = Integer.valueOf(scanner.nextLine());
+        System.out.print("Enter the second number: ");
+        int num2 = Integer.valueOf(scanner.nextLine());
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2);
+        } else if (num2 > num1) {
+            System.out.println(num1 + " is smaller than " + num2);
+        } else {
+            System.out.println(num1 + " is equal to " + num2);
+        }
     }
 }
