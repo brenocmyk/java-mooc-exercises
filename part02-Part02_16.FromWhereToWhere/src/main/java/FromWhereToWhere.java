be645795df64d4eebed73e421ad6c1bc62825df5
breno <breno.aguiarba06@gmail.com>
2026-03-07 16:10:16 -0300

    atualizando exercicios

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -5,6 +5,12 @@ public class FromWhereToWhere {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.println("Where to? ");
+        int to = Integer.valueOf(scanner.nextLine());
+        System.out.println("Where from? ");
+        int from = Integer.valueOf(scanner.nextLine());
+        for (int i = from; i <= to; i++) {
+            System.out.println(i);
+        }
     }
 }
--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,12 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Give a number:");
+        int lastNumber = Integer.valueOf(scanner.nextLine());
+        int sum = 1;
+        for (int i = 1; i <= lastNumber; i++) {
+            sum = sum*i;
+        }
+        System.out.println("Factorial: " + sum);
     }
 }
